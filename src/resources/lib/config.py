33ddab5055a06dcc7c54b48f542bdc493b9146ca
thoradia <22841905+thoradia@users.noreply.github.com>
2020-12-11 22:09:37 +0100

    config.py: cleanup

--- src/resources/lib/config.py
+++ src/resources/lib/config.py
@@ -1,13 +1,8 @@
 # SPDX-License-Identifier: GPL-2.0
 # Copyright (C) 2020-present Team LibreELEC
 
-import asyncio
-import dbussy
 import os
-import ravel
-import threading
-import traceback
-import xbmc
+import log
 import xbmcaddon
 import xbmcgui
 
@@ -15,28 +10,9 @@ ADDON = xbmcaddon.Addon()
 ADDON_ICON = ADDON.getAddonInfo('icon')
 ADDON_NAME = ADDON.getAddonInfo('name')
 
-BUS = ravel.system_bus()
-
 CONFIG_CACHE = os.environ.get('CONFIG_CACHE', '/storage/.cache')
 
-LOG_HEADER = f'{ADDON_NAME}:'
-LOG_LEVEL = xbmc.LOGDEBUG
 
-
-def log_function(function):
-    def wrapper(*args, **kwargs):
-        header = f'{LOG_HEADER}{function.__qualname__}'
-        try:
-            xbmc.log(f'{header}<{args}{kwargs}', LOG_LEVEL)
-            result = function(*args, **kwargs)
-            xbmc.log(f'{header}>{result}', LOG_LEVEL)
-            return result
-        except Exception as e:
-            xbmc.log(f'{header}#{repr(e)}', xbmc.LOGERROR)
-            xbmc.log(traceback.format_exc(), xbmc.LOGERROR)
-    return wrapper
-
-
-@log_function
+@log.log_function()
 def notification(message, heading=ADDON_NAME, icon=ADDON_ICON):
     xbmcgui.Dialog().notification(heading, message, icon)
